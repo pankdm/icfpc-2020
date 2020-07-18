c7a9842d3f4aa639e2b548d5e1ff7073966a5f5a
Dmitry Panin <pank.dm@gmail.com>
2020-07-17 23:09:51 -0700

    Annotate + whitelist

--- src/annotate.py
+++ src/annotate.py
@@ -1,6 +1,8 @@
 
 import sys
 import re
+import os
+import time
 from decode import parse_program, read_source, sorted_defs
 
 from funcs import *
@@ -8,15 +10,26 @@ from funcs import *
 def sorted_by_id(defs):
   return sorted(defs.items(), key=lambda _def: _def[0])
 
+def create_file(it):
+  timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+  # datetime.utcnow().isoformat()
+  # folder = "annotations/tmp/{}".format(timestamp)
+  folder = "annotations/tmp"
+  os.makedirs(folder, exist_ok=True)
+  filepath = "{}/{:03d}.txt".format(folder, it)
+  return open(filepath, 'w')
 
-def try_eval(expr):
+def try_eval(expr, whitelist):
   if expr.name.startswith(":"):
+    maybe_eval = whitelist.get(expr.name, None)
+    if maybe_eval:
+      return maybe_eval
     raise RuntimeError("Dereferencing is not supported")
   if re.match('-?\d+', expr.name):
     return int(expr.name)
   if expr.name == "ap":
-    left = try_eval(expr.args[0])
-    right = try_eval(expr.args[1])
+    left = try_eval(expr.args[0], whitelist)
+    right = try_eval(expr.args[1], whitelist)
     return AP(left, right)
   else:
     func = FUNCTIONS.get(expr.name, None)
@@ -24,8 +37,6 @@ def try_eval(expr):
     return func
 
   
-
-
 class Expr:
   def __init__(self, name, args):
     self.name = name
@@ -63,20 +74,30 @@ def parse_next_expr(s):
   return Expr(token, [])
 
 
+def simplification(sorted_defs):
+  whitelist = {}
+  for it in range(10):
+    current_whitelist = {}
+    with create_file(it) as output:
+      for name, tokens in sorted_defs:
+        stream = TokenStream(tokens)
+        expr = parse_next_expr(stream)
+        dump = expr.dump()
+
+        try:
+          maybe_eval = try_eval(expr, whitelist)
+          current_whitelist[name] = maybe_eval
+        except RuntimeError:
+          maybe_eval = "???"
+        output.write(f'{name} = {dump}\n')
+        output.write(f'      {maybe_eval}\n')
+    whitelist.update(current_whitelist)
+
+
 if __name__ == "__main__":
   sys.setrecursionlimit(10000)
   defs = parse_program(read_source(sys.argv[1]))
   sorted_defs = sorted_defs(defs)
-  for name, tokens in sorted_defs:
-    stream = TokenStream(tokens)
-    expr = parse_next_expr(stream)
-    dump = expr.dump()
-
-    try:
-      maybe_eval = try_eval(expr)
-    except RuntimeError:
-      maybe_eval = "???"
-    print (f'{name} = {dump}')
-    print (f'      {maybe_eval}')
+  simplification(sorted_defs)
 
 
