3eb46b688d914d51872f88c46a0de366fbd5e329
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2020-07-18 00:22:28 -0700

    Update annotate.py to try a naive recursive eval approach.
    try_eval now tries to resolve recursive definitions and evaluate them.
    
    Evaluating galaxy now hangs with the following call depth map:
    
    Descending into :1141, {':1338': 1, ':1342': 1, ':1343': 44, ':1141': 555}

--- src/annotate.py
+++ src/annotate.py
@@ -21,17 +21,34 @@ def create_file(it):
   filepath = "{}/{:03d}.txt".format(folder, it)
   return open(filepath, 'w')
 
-def try_eval(expr, whitelist):
+def expr_for_tokens(tokens):
+  stream = TokenStream(tokens)
+  expr = parse_next_expr(stream)
+  return expr
+
+def try_eval(expr, whitelist, allow_recursion=False, defs={}, recursion_depth_map={}):
   if expr.name.startswith(":"):
     maybe_eval = whitelist.get(expr.name, None)
     if maybe_eval:
-      return maybe_eval
-    raise RuntimeError("Dereferencing is not supported")
+        return maybe_eval
+    if allow_recursion:
+      print(f"Descending into {expr}, {recursion_depth_map}")
+      if recursion_depth_map.get(expr.name, 0) > 1000:
+        # raise RuntimeError(f"Recursion deeper than 1000, expr={expr}")
+
+        # Returning zero just to see if we can progress elsewhere.
+        return 0
+      sub_expr = expr_for_tokens(defs[expr.name])
+      new_recursion_depth_map = dict(recursion_depth_map)
+      new_recursion_depth_map[expr.name] = recursion_depth_map.get(expr.name, 0) + 1
+      return try_eval(sub_expr, whitelist, allow_recursion=allow_recursion, defs=defs, recursion_depth_map=new_recursion_depth_map)
+    else:
+      raise RuntimeError("Dereferencing is not supported")
   elif re.match(r'-?\d+', expr.name):
     return int(expr.name)
   elif expr.name == "ap":
-    left = try_eval(expr.args[0], whitelist)
-    right = try_eval(expr.args[1], whitelist)
+    left = try_eval(expr.args[0], whitelist, allow_recursion=allow_recursion, defs=defs, recursion_depth_map=recursion_depth_map)
+    right = try_eval(expr.args[1], whitelist, allow_recursion=allow_recursion, defs=defs, recursion_depth_map=recursion_depth_map)
     return AP(left, right)
   else:
     func = FUNCTIONS.get(expr.name, None)
@@ -94,12 +111,28 @@ def simplification(sorted_defs):
         output.write(f'{name} = {dump}\n')
         output.write(f'      {maybe_eval}\n')
     whitelist.update(current_whitelist)
+  
+  return whitelist
+
+  def honest_eval(def_to_eval, defs, args, whitelist):
+    tokens = defs[def_to_eval]
 
 
 if __name__ == "__main__":
   sys.setrecursionlimit(10000)
   defs = parse_program(read_source(sys.argv[1]))
-  sorted_defs = sorted_defs(defs)
-  simplification(sorted_defs)
+
+  run_simplification=True
+  if run_simplification:
+    sorted_defs = sorted_defs(defs)
+    whitelist = simplification(sorted_defs)
+  else:
+    # Try to run the naive recursive evaluation. See "return 0" above, where to try to abort recusions that are too deep.
+    # Currently it hangs with the following expr call depths:
+    #   Descending into :1141, {':1338': 1, ':1342': 1, ':1343': 44, ':1141': 555}
+    result = try_eval(expr_for_tokens(["ap", "ap", ":1338", "0", "0"]), whitelist={}, allow_recursion=True, defs=defs)
+    print("result={result}")
+
+
 
 
