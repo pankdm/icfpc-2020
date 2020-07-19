b27ed46b1d0c4878cffde6c81eeb09136bb88177
Victor Klochikhin <vintlucky777@gmail.com>
2020-07-19 00:19:06 -0700

    faster more efficient rewind

--- src/ui.py
+++ src/ui.py
@@ -23,6 +23,7 @@ class TkUI:
     self.canvas.bind("<Shift-A>", self.handle_click_all_pixels)
     self.canvas.bind("<BackSpace>", self.handle_rewind)
     self.canvas.pack()
+    self.current_img_data = None
 
     # Initial states:
     #
@@ -53,18 +54,13 @@ class TkUI:
     # end game?
     # self.current_state = list_to_cons([10, [], 8, []])
 
-    self.state_history = []
+    self.state_click_history = []
+    self.img_history = [None]
     self.interact(0, 0)
 
   def handle_rewind(self, event):
       print(f'\n\nAttempting time travel...')
       self.rewind_state()
-      short_display_len = 3
-      state_history_slice = self.state_history[-short_display_len:]
-      prefix = '... ' if len(state_history_slice) > short_display_len else ''
-      state_history_repr = "; ".join([f':state {recursive_list_convert(state)} :click {click}' for state, click in state_history_slice])
-      state_history_repr = f'<{prefix}{state_history_repr} '
-      print(f'current state stack: {state_history_repr}')
 
   def handle_click_all_pixels(self, event):
       print(f"click all pixels")
@@ -92,33 +88,37 @@ class TkUI:
       self.interact(x, y)
       self.canvas.focus_set()
 
-  def update_state(self, current_state, click, next_state):
+  def update_state(self, current_state, img_data, click, next_state, next_img_data):
       if recursive_list_convert(current_state) != recursive_list_convert(next_state):
-          self.state_history.append([current_state, click])
+          self.state_click_history.append((current_state, click))
+          self.img_history.append(img_data)
           self.current_state = next_state
+          self.current_img_data = next_img_data
 
   def rewind_state(self):
-      if (len(self.state_history) > 1):
+      if (len(self.state_click_history) > 1):
           print('Rewind state, time travel, Morty!')
-          prev_state, last_click = self.state_history.pop()
-          pre_prev_state, prev_click = self.state_history.pop()
-          self.current_state = pre_prev_state
-          self.interact(*prev_click)
+          prev_state, prev_click = self.state_click_history.pop()
+          prev_img_data = self.img_history.pop()
+          self.current_state = prev_state
+          self.current_img_data = prev_img_data
+          self.draw()
       else:
           print('Already at the earliest state.')
 
+
+  def draw(self):
+      # clean canvas before draw
+      self.canvas.delete(ALL)
+      # quick, draw!
+      multipledraw_helper(self.current_img_data, draw_dot_impl=self.add_pixel)
 
   def interact(self, x, y):
       click = Ap(Ap(cons, Atom(str(x))), Atom(str(y)))
       (new_state, img_data) = interact(PROTOCOL, self.current_state, click)
       # print(f"new_state = {new_state} img_data={img_data}")
-      self.current_img_data = img_data
-      self.update_state(self.current_state, (x, y), new_state)
-
-      # clean canvas before draw
-      self.canvas.delete(ALL)
-      # quick, draw!
-      multipledraw_helper(img_data, draw_dot_impl=self.add_pixel)
+      self.update_state(self.current_state, self.current_img_data, (x, y), new_state, img_data)
+      self.draw()
 
   def add_pixel(self, x, y, index):
     # colors = ["white", "blue", "red", "green"]
