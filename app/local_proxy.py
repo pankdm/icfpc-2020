db97e88b95663e3c8a394173150e3ec9298f3210
Dmitry Panin <pank.dm@gmail.com>
2020-07-19 15:38:01 -0700

    interactive draw + gravity grid

--- app/local_proxy.py
+++ app/local_proxy.py
@@ -11,6 +11,9 @@ from bots import *
 from models import *
 from player import *
 
+from space_viz import SpaceUI
+
+
 KONTUR_URL = "https://icfpc2020-api.testkontur.ru"
 API_KEY = "apiKey=6e1336a2ffa94971b5f74715a85708b9"
 
@@ -21,28 +24,34 @@ def main():
     # a = [1, [1, [[256, [0, [[512, [1, [64, None]]], [[16, [128, None]], [[446, [0, [0, [1, None]]]], None]]]]], [[0, [[16, [128, None]], [[[[1, [0, [[-48, -29], [[0, 0], [[446, [0, [0, [1, None]]]], [0, [64, [1, None]]]]]]]], [None, None]], [[[0, [1, [[48, 29], [[0, 0], [[446, [0, [0, [1, None]]]], [0, [64, [1, None]]]]]]]], [None, None]], None]], None]]], None]]]]
     # print (flatten_cons(a))
     # return
+
+    responses = []
+    ui = SpaceUI(responses)
+
     full_url = f'{KONTUR_URL}/aliens/send?{API_KEY}'
 
     proxy = Proxy(full_url)
     create = proxy.create_new_game()
-
     print (f"keys: {create}")
     key1 = create[1][0][1]
     key2 = create[1][1][1]
 
     bot1 = DoNothingBot()
-    bot2 = ShooterBot()
+    bot2 = FlyingBot()
 
-    player1 = Player(proxy, key1, bot1, log=True, display_name="Player 1")
+    player1 = Player(proxy, key1, bot1, log=True, display_name="Player 1", ui = ui)
     player2 = Player(proxy, key2, bot2, display_name="Player 2")
 
     task1 = lambda : player_loop(player1)
     task2 = lambda : player_loop(player2)
 
-    t = threading.Thread(target=task1)
-    t.start()
+    t1 = threading.Thread(target=task1)
+    t1.start()
 
-    task2()
+    t2 = threading.Thread(target=task2)
+    t2.start()
+
+    ui.mainloop()
 
 if __name__ == '__main__':
     main()
