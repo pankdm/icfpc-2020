4b07df4887b08a01aac015d115ddae3aa6b2b50d
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2020-07-19 23:35:42 -0700

    Enable TrajectoryBot in main

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def main():
     proxy = Proxy(full_url)
 
     # NOTE: fix here for different algorithms
-    bot = FlyingBot()
+    bot = TrajectoryBot()
 
     player = Player(proxy, player_key, bot, display_name="Me")
     player_loop(player)
