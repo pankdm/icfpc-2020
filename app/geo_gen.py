4db516095fb184b3a9ddf806a05c6027a1cf509a
Oleg Godovykh <0legg@users.noreply.github.com>
2020-07-20 01:01:49 -0700

    Some updates to orbital exploration

--- app/geo_gen.py
+++ app/geo_gen.py
@@ -9,14 +9,14 @@ def check_geo(coord, speed, accel):
     num_ticks = 256
     state = KinematicState(coord, speed).update(accel)
     for _ in range(num_ticks):
-        if abs(state.pos[0]) <= 16 and abs(state.pos[1]) <= 16:
+        if abs(state.pos[0]) <= 20 and abs(state.pos[1]) <= 20:
             return False
         state = state.update()
     return True
 
 def main():
-    coords = [(x, y) for (x, y) in itertools.product(range(-48, 49), repeat=2) if abs(x) > 16 or abs(y) > 16]
-    speeds = [(x, y) for (x, y) in itertools.product(range(-10, 11), repeat=2) if abs(x) + abs(y) < 10]
+    coords = [(x, y) for (x, y) in itertools.product(range(-48, 49), repeat=2) if abs(x) > 20 or abs(y) > 20]
+    speeds = [(x, y) for (x, y) in itertools.product(range(-10, 11), repeat=2) if abs(x) + abs(y) < 8]
     accels = [(x, y) for (x, y) in itertools.product(range(-1, 2), repeat=2)]
 
     for coord in coords:
